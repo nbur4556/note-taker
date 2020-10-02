6cc17afa8fcb536aaa245073f5072f1b997305eb
nbur4556 <nbur4556@gmail.com>
2020-10-02 07:15:01 -0500

    created endpoints for home and notes pages

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 
 let PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+app.get('/notes', (req, res) => {
+    res.send('<h1>Hello Notes</h1>');
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-})
+    console.log(`Listening on port ${PORT}`);
+});

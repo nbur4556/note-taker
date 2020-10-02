738f0462a8357109e342668abc370a7eaf4e26b6
nbur4556 <nbur4556@gmail.com>
2020-10-02 07:07:08 -0500

    created local server on port 3000

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+
+let PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+})

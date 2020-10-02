c37a8ef805fb4da104e1384440c7268226f04f71
nbur4556 <nbur4556@gmail.com>
2020-10-02 07:39:40 -0500

    sets get endpoint for returning notes as API data

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const app = express();
 
 let PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// PAGE ENDPOINT
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.get('/notes', (req, res) => {
     res.sendFile(`${__dirname}/public/notes.html`);
+});
+
+// API ENDPOINT
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+
+        res.json(data);
+    });
 });
 
 app.listen(PORT, () => {
